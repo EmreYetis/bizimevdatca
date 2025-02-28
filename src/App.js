59f99525ed0483826e4caed13ccbd329d3581157
Emre Yetis <emreyetis955@gmail.com>
2025-02-28 13:51:39 +0300

    v2
    v222

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { useState } from 'react';
 import CheckIn from './pages/CheckIn';
 import CheckOut from './pages/CheckOut';
 import Report from './pages/Report';
@@ -8,44 +8,70 @@ import ReservationConfirmation from './pages/ReservationConfirmation';
 import KapiGiris from './pages/KapiGiris';
 
 function App() {
+  const [selectedComponent, setSelectedComponent] = useState(null);
+
+  const renderComponent = () => {
+    switch(selectedComponent) {
+      case 'checkin':
+        return <CheckIn />;
+      case 'checkout':
+        return <CheckOut />;
+      case 'report':
+        return <Report />;
+      case 'reservation':
+        return <ReservationConfirmation />;
+      case 'kapigiris':
+        return <KapiGiris />;
+      default:
+        return null;
+    }
+  };
+
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div className="App container text-center">
-        <h1>BED Resepsiyon</h1>
-        <Routes>
-          <Route path="/" element={
-            <div className="row mt-5">
-              <div className="col-md-6">
-                <Link to="/checkin">
-                  <button className="btn btn-success btn-block mb-3">Check In</button>
-                </Link>
-                <Link to="/checkout">
-                  <button className="btn btn-danger btn-block mb-3">Check Out</button>
-                </Link>
-              </div>
-              <div className="col-md-6">
-                <Link to="/report">
-                  <button className="btn btn-warning btn-block mb-3">Rapor</button>
-                </Link>
-                <Link to="/reservation">
-                  <button className="btn btn-primary btn-block mb-3">Rezervasyon Onay</button>
-                </Link>
-              </div>
-              <div className='col-md-6'>
-                <Link to="/kapigiris">
-                  <button className="btn btn-info btn-block mb-3">Kapı Müşterisi</button>
-                </Link>
-              </div>
-            </div>
-          } />
-          <Route path="/checkin" element={<CheckIn />} />
-          <Route path="/checkout" element={<CheckOut />} />
-          <Route path="/report" element={<Report />} />
-          <Route path="/reservation" element={<ReservationConfirmation />} />
-          <Route path="/kapigiris" element={<KapiGiris />} />
-        </Routes>
+    <div className="App container text-center">
+      <h1>BED Resepsiyon</h1>
+      <div className="row mt-5">
+        <div className="col-md-6">
+          <button 
+            className="btn btn-success btn-block mb-3" 
+            onClick={() => setSelectedComponent('checkin')}
+          >
+            Check In
+          </button>
+          <button 
+            className="btn btn-danger btn-block mb-3"
+            onClick={() => setSelectedComponent('checkout')}
+          >
+            Check Out
+          </button>
+        </div>
+        <div className="col-md-6">
+          <button 
+            className="btn btn-warning btn-block mb-3"
+            onClick={() => setSelectedComponent('report')}
+          >
+            Rapor
+          </button>
+          <button 
+            className="btn btn-primary btn-block mb-3"
+            onClick={() => setSelectedComponent('reservation')}
+          >
+            Rezervasyon Onay
+          </button>
+        </div>
+        <div className='col-md-6'>
+          <button 
+            className="btn btn-info btn-block mb-3"
+            onClick={() => setSelectedComponent('kapigiris')}
+          >
+            Kapı Müşterisi
+          </button>
+        </div>
       </div>
-    </Router>
+      <div className="mt-4">
+        {renderComponent()}
+      </div>
+    </div>
   );
 }
 
