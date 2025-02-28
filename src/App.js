5ef93944387058608a0211f8d871113f2875bb2e
Emre Yetis <emreyetis955@gmail.com>
2025-02-28 13:29:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,33 +9,35 @@ import KapiGiris from './pages/KapiGiris';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App container text-center">
         <h1>BED Resepsiyon</h1>
-        <div className="row mt-5">
-          <div className="col-md-6">
-            <Link to="/checkin">
-              <button className="btn btn-success btn-block mb-3">Check In</button>
-            </Link>
-            <Link to="/checkout">
-              <button className="btn btn-danger btn-block mb-3">Check Out</button>
-            </Link>
-          </div>
-          <div className="col-md-6">
-            <Link to="/report">
-              <button className="btn btn-warning btn-block mb-3">Rapor</button>
-            </Link>
-            <Link to="/reservation">
-              <button className="btn btn-primary btn-block mb-3">Rezervasyon Onay</button>
-            </Link>
-          </div>
-          <div className='col-md-6'>
-          <Link to="/kapigiris">
-              <button className="btn btn-info btn-block mb-3">Kapı Müşterisi</button>
-            </Link>
-          </div>
-        </div>
         <Routes>
+          <Route path="/" element={
+            <div className="row mt-5">
+              <div className="col-md-6">
+                <Link to="/checkin">
+                  <button className="btn btn-success btn-block mb-3">Check In</button>
+                </Link>
+                <Link to="/checkout">
+                  <button className="btn btn-danger btn-block mb-3">Check Out</button>
+                </Link>
+              </div>
+              <div className="col-md-6">
+                <Link to="/report">
+                  <button className="btn btn-warning btn-block mb-3">Rapor</button>
+                </Link>
+                <Link to="/reservation">
+                  <button className="btn btn-primary btn-block mb-3">Rezervasyon Onay</button>
+                </Link>
+              </div>
+              <div className='col-md-6'>
+                <Link to="/kapigiris">
+                  <button className="btn btn-info btn-block mb-3">Kapı Müşterisi</button>
+                </Link>
+              </div>
+            </div>
+          } />
           <Route path="/checkin" element={<CheckIn />} />
           <Route path="/checkout" element={<CheckOut />} />
           <Route path="/report" element={<Report />} />
